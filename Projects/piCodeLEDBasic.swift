99246e08c8b6bdaf0574ca04891950c02e2c9063
futurejones <neil@futurejonesapps.com>
2016-10-26 09:25:24 +0900

    Add piCodeGPIO module Add piCodeLED projects

---
+++ Projects/piCodeLEDBasic.swift
@@ -0,0 +1,36 @@
+// Created with PiCode for Swift 3.0
+// A Swift-Lite project file
+// type:project
+// name:piCodeLEDBasic
+// include:piCodeGPIO.swift
+
+import Glibc
+
+// auto detect board type (detects all boards with 40 GPIO pins)
+let gpios = autoDetectBoardType()
+
+// for older boards with 26 GPIO pins please set board type manually
+//let gpios = PiCodeGPIO.RPIRev1
+//let gpios = PiCodeGPIO.RPIRev2
+
+// set output pin
+var gp = gpios[.P17]!
+gp.direction = .OUT
+gp.value = 1
+
+var flashNumber = 10
+
+repeat{
+       print("flash on")
+       usleep(500*1000)
+       gp.value = 1
+
+       print("flash off")
+       usleep(500*1000)
+       gp.value = 0
+
+     flashNumber -= 1
+
+} while(flashNumber > 0)
+
+
