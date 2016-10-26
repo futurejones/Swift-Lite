099ea6cd5cbd3aff642922633ce96d36361a5151
futurejones <neil@futurejonesapps.com>
2016-10-26 17:11:28 +0900

    Update timers

--- Projects/piCodeLEDTimer.swift
+++ Projects/piCodeLEDTimer.swift
@@ -5,6 +5,7 @@
 // include:piCodeGPIO.swift
 
 import Glibc
+import Foundation
 
 // auto detect board type (detects all boards with 40 GPIO pins)
 let gpios = autoDetectBoardType()
@@ -18,19 +19,42 @@ var gp = gpios[.P17]!
 gp.direction = .OUT
 gp.value = 1
 
-var flashNumber = 10
 
-repeat{
-       print("flash on")
-       usleep(500*1000)
-       gp.value = 1
+var  intervalTimer: Timer!
+var flashTimer: Timer!
 
-       print("flash off")
-       usleep(500*1000)
-       gp.value = 0
+var flashNumber = 5
 
-     flashNumber -= 1
 
-} while(flashNumber > 0)
+func runIntervalTimer() {
+    intervalTimer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true, block: {(_) in flashOnLED()})
+}
+func runflashTimer() {
+    flashTimer = Timer.scheduledTimer(withTimeInterval: 0.75, repeats: false, block: {(_) in flashOffLED()})
+}
 
+func flashOnLED() {
+    print("flash on")
+    gp.value = 1
+    runflashTimer()    
+}
+
+func flashOffLED() {
+    gp.value = 0
+    print("flash off")
+    flashNumber -= 1
+    if flashNumber == 0 {
+        intervalTimer.invalidate()
+        print("finished")
+    }
+    
+}
+
+runIntervalTimer()
+
+let flashLEDLoop = RunLoop.current
+flashLEDLoop.add(intervalTimer, forMode: .defaultRunLoopMode)
+
+
+flashLEDLoop.run(until: Date(timeIntervalSinceNow: 11))
 
