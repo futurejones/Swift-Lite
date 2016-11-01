1890ce9b3deb3aeff42e6d6219512856d8ac746c
futurejones <neil@futurejonesapps.com>
2016-11-01 15:17:24 +0900

    update piCodeLEDTimer

--- Projects/piCodeLEDTimer.swift
+++ Projects/piCodeLEDTimer.swift
@@ -1,10 +1,10 @@
+
 // Created with PiCode for Swift 3.0
 // A Swift-Lite project file
 // type:project
-// name:piCodeflashLED
+// name:piCodeLEDTimer
 // include:piCodeGPIO.swift
 
-import Glibc
 import Foundation
 
 // auto detect board type (detects all boards with 40 GPIO pins)
@@ -14,29 +14,37 @@ let gpios = autoDetectBoardType()
 //let gpios = PiCodeGPIO.RPIRev1
 //let gpios = PiCodeGPIO.RPIRev2
 
-// set output pin
-var gp = gpios[.P17]!
-gp.direction = .OUT
-gp.value = 1
+// setup the GPIO pins
+var gp = gpios[.P17]!   // Pin to conect the LED to
+gp.direction = .OUT     // Pin connection direction - OUT - when turned on the pin will output 5v
+gp.value = 0            // Set intial value 0 = off 1 = on
 
 
-var  intervalTimer: Timer!
-var flashTimer: Timer!
+var intervalTimer: Timer!   // this timer controls the time intervals the LED turns on. As this is a repeating timer it needs to be added to a runloop.
+var flashTimer: Timer!      // this timer controls the length of time the LED remains on.
 
-var flashNumber = 5
+var flashNumber = 5 // the number of flashes
+
+var flashIntervalTime: Double = 2.0 // the time between the start of each flash
+
+var flashLengthTime: Double = 0.75 // the length of time of each flash
+
+var runLoopTime: Double = 11 // the lenghth of time the runloop will run - set this to be longer than the time for all the flashes to complete
+// the runloop can be set to continous if the is action you need.
 
 
 func runIntervalTimer() {
-    intervalTimer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true, block: {(_) in flashOnLED()})
+    intervalTimer = Timer.scheduledTimer(withTimeInterval: flashIntervalTime, repeats: true, block: {(_) in flashOnLED()})
 }
+
 func runflashTimer() {
-    flashTimer = Timer.scheduledTimer(withTimeInterval: 0.75, repeats: false, block: {(_) in flashOffLED()})
+    flashTimer = Timer.scheduledTimer(withTimeInterval: flashLengthTime, repeats: false, block: {(_) in flashOffLED()})
 }
 
 func flashOnLED() {
     print("flash on")
     gp.value = 1
-    runflashTimer()    
+    runflashTimer()
 }
 
 func flashOffLED() {
@@ -47,7 +55,6 @@ func flashOffLED() {
         intervalTimer.invalidate()
         print("finished")
     }
-    
 }
 
 runIntervalTimer()
@@ -55,6 +62,5 @@ runIntervalTimer()
 let flashLEDLoop = RunLoop.current
 flashLEDLoop.add(intervalTimer, forMode: .defaultRunLoopMode)
 
-
-flashLEDLoop.run(until: Date(timeIntervalSinceNow: 11))
+flashLEDLoop.run(until: Date(timeIntervalSinceNow: runLoopTime))
 
